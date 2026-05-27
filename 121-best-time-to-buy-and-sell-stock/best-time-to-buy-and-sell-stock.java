d344e638c4253363fd64367b3ed256c1e7208c53
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-05-27 22:44:57 +0530

    Fix formatting issues in stock profit calculation

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -5,7 +5,7 @@ class Solution {
 
         for (int price : prices) {
             if (price < minPrice) {
-                minPrice = price; 
+                minPrice = price;  
             } else if (price - minPrice > maxProfit) {
                 maxProfit = price - minPrice; 
             }
