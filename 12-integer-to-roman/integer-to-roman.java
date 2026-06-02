7910d56884b5aaf7a55f645464c57eaa6b8fa5e9
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-06-02 19:03:00 +0530

    Correct StringBuilder initialization in intToRoman
    Fix formatting issue in integerToRoman method.

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -3,7 +3,7 @@ class Solution {
         final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         final String[] symbols = {"M", "CM", "D",  "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(); 
 
         for (int i = 0; i < values.length; ++i) {
             if (num == 0) 
