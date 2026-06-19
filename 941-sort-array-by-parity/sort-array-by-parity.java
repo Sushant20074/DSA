4e91346526f0cf9bc1f93cda87e78cb35c2f0eb7
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-06-19 15:51:38 +0530

    Fix sorting logic in sortArrayByParity method

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -3,7 +3,7 @@ class Solution {
         int left=0;
         for(int i=0; i<nums.length; i++){
             if(nums[i]%2==0){
-            int temp=nums[left];
+            int temp=nums[left]; 
             nums[left]=nums[i];
             nums[i]=temp;
             left++;
