994c65206813675e2ddd342eebc5f68a3f61e325
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-06-19 15:55:59 +0530

    Fix binary search mid calculation syntax

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -2,7 +2,7 @@ class Solution {
     public int search(int[] nums, int target) {
         int l = 0, r = nums.length-1;
         while (l <= r){
-            int mid = l + (r - l)/2;
+            int mid = l + (r - l)/2; 
             if (nums[mid] == target) return mid;
             else if (nums[mid] < target) l = mid + 1;
             else r = mid - 1;
