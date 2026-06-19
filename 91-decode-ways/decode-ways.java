b6d1874d1252cb030a1c447c2f7cb461f052b187
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-06-19 15:52:35 +0530

    Fix formatting issues in decode-ways.java

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -5,7 +5,7 @@ class Solution {
         } 
 
         int n = s.length();
-        int[] dp = new int[n + 1];
+        int[] dp = new int[n + 1]; 
         dp[0] = dp[1] = 1;
 
         for (int i = 2; i <= n; i++) {
