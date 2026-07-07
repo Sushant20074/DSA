a22a19090ccd021d4b15b55fc9be679fb4114e6b
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-07-07 22:04:52 +0530

    Fix formatting issue in hasPathSum method

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -16,7 +16,7 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) return false;
-        if (root.left == null && root.right == null) {
+        if (root.left == null && root.right == null) { 
             return targetSum - root.val == 0;
         }
 
