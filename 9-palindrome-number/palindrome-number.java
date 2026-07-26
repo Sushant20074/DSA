90d4dda3e75b72a7f6d15b49414bee790af5981c
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-07-26 21:40:12 +0530

    Fix formatting and clean up palindrome logic

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -5,7 +5,7 @@ class Solution {
         }
         int original = x;
         int reverse = 0; 
-
+ 
         while (x > 0) {
             int digit = x % 10;        
             reverse = reverse * 10 + digit;
