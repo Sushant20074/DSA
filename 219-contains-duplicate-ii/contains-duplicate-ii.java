d2dc38d6efa0ef595c66f4d777e6e4ec5b829f8b
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-07-27 23:00:33 +0530

    Fix logic to check for duplicates within range k

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -4,7 +4,7 @@ class Solution {
             for(int j=i+1; j<=i+k && j<nums.length; j++){
                 if(nums[i]==nums[j]){
                     return true;
-                }
+                } 
             }
         }
         return false;
