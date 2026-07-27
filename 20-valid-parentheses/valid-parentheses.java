a6d2157e3e58a918febe25fed80b32939a790f2d
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-07-27 23:00:16 +0530

    Refactor isValid method to simplify parentheses check

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,7 +2,7 @@ public class Solution {
     public static boolean isValid(String s) {
         while (true) {
             if (s.contains("()")) {
-                s = s.replace("()", "");
+                s = s.replace("()", ""); 
             } else if (s.contains("{}")) {
                 s = s.replace("{}", "");
             } else if (s.contains("[]")) {
