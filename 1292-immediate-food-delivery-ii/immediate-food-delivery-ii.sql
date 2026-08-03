6dfc7fc4fddbdff6e046cc559c41a86433ab21f1
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-08-03 20:08:51 +0530

    Fix SQL query for immediate food delivery percentage

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -2,7 +2,7 @@
 Select round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
 from Delivery
 where (customer_id, order_date) in (
-  Select customer_id, min(order_date) 
+  Select customer_id, min(order_date)  
   from Delivery
   group by customer_id
 );
