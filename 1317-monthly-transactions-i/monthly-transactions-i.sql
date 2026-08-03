9f2f7f68da5bbd84eb18392daa1e5f2fdc359d67
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-08-03 20:09:23 +0530

    Fix formatting of trans_total_amount calculation

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -4,7 +4,7 @@ SELECT
     country, 
     COUNT(id) AS trans_count,
     SUM(state = 'approved') AS approved_count,
-    SUM(amount) AS trans_total_amount,
+    SUM(amount) AS trans_total_amount, 
     SUM((state = 'approved') * amount) AS approved_total_amount
 FROM  
     Transactions
