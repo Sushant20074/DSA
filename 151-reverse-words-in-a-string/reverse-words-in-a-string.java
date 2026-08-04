fa50d369bbd47529759c4f56ac6651896346c97b
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-08-04 22:07:35 +0530

    Fix spacing in reverse words function

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -4,7 +4,7 @@ class Solution {
         String[] words = s.split("\\s+");
          StringBuilder reversed = new StringBuilder();
         for (int i = words.length - 1; i >= 0; i--) {
-            reversed.append(words[i]); 
+            reversed.append(words[i]);  
             if (i != 0) { 
                 reversed.append(" ");
             }
