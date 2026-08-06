c8cff18dbed80279a662b289dfae84f30856d298
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-08-06 22:01:26 +0530

    Fix formatting issues in container-with-most-water.java

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -5,7 +5,7 @@ class Solution {
         int right = height.length - 1;
         while (left < right) {
             maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
-
+ 
             if (height[left] < height[right]) { 
                 left++;
             } else { 
