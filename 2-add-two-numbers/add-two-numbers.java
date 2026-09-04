059399fdf57c24fbeeaf83dd987898c5a133686b
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-09-04 22:12:09 +0530

    Fix formatting issue in add-two-numbers.java

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -6,7 +6,7 @@ class Solution {
         int carry = 0;
 
         while (l1 != null || l2 != null || carry != 0) {
-            int digit1 = (l1 != null) ? l1.val : 0;
+            int digit1 = (l1 != null) ? l1.val : 0; 
             int digit2 = (l2 != null) ? l2.val : 0;
 
             int sum = digit1 + digit2 + carry;
