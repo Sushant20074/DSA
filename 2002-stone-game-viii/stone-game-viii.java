17dd38a5f05099013497faea393324113d79ea0c
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-09-12 16:53:06 +0530

    Fix loop condition in stone game logic

--- 2002-stone-game-viii/stone-game-viii.java
+++ 2002-stone-game-viii/stone-game-viii.java
@@ -5,7 +5,7 @@ class Solution {
             A[i] += A[i - 1];
 
         int ans = A[n - 1];
-        for (int i = n - 2; i > 0; i--)
+        for (int i = n - 2; i > 0; i--) 
             ans = Math.max(ans, A[i] - ans);
 
         return ans;
