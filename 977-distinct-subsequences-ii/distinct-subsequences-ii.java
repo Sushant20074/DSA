46f64b1a8bc8e53f1b6cc726b49b4e2fda79d39f
Sushant Kumar <142523508+Sushant20074@users.noreply.github.com>
2026-09-14 22:50:14 +0530

    Fix formatting issues in distinct subsequences II method

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.java
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.java
@@ -5,7 +5,7 @@ class Solution {
         int tot = 0;
         int[] dp = new int[26];
 
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) { 
             int c = s.charAt(i) - 97;
             int add = (tot - dp[c] + MOD) % MOD;
 
